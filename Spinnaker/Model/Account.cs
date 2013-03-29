bbefa3e13d229c75772862a0cbe3f9d15f9ef0c0
lI' Ghun <info@li-ghun.de>
2013-03-29 17:53:42 +0100

    Added basic classes for accounts, drafts and the outbox, changed .NET to 4.0 full as library needs it

---
+++ Spinnaker/Model/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppNetDotNet.Model;
+using AppNetDotNet.ApiCalls;
+
+namespace Spinnaker.Model
+{
+    public class Account
+    {
+        public string access_token { get; set; }
+        public Token token { get; set; }
+        public User user { get; set; }
+
+        public string username
+        {
+            get
+            {
+                if (user != null)
+                {
+                    return user.username;
+                }
+                else
+                {
+                    return "Unauthorized";
+                }
+            }
+
+        }
+
+
+        public bool send_post(string text, string local_file_to_embed = "")
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                Posts.create(this.access_token, text);
+            }
+            return false;
+        }
+    }
+}
---
+++ Spinnaker/Model/Draft.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spinnaker.Model
+{
+    public class Draft
+    {
+        public string  text { get; set; }
+
+        public bool send_now(Account account)
+        {
+            if (account != null)
+            {
+                return account.send_post(text);
+            }
+            return false;
+        }
+    }
+}
